dea1b0852cd270c19c56bb98b523eb75de57b7d4
Rubén Gámez Torrijos <19588354+RubenGamezTorrijos@users.noreply.github.com>
2021-08-29 19:11:33 +0200

    Update index.py

--- app-web-python/index.py
+++ app-web-python/index.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route()
 def Inicio(/Inicio):
-  return 'Hola Mundo'
+  return render_template ('Inicio.html')
 def Sobre Mi(/SobreMi):
   return 'Sobre mí'
 
