6bf7f1d7b35de846899af6d2086b6e44185af179
Rubén Gámez Torrijos <19588354+RubenGamezTorrijos@users.noreply.github.com>
2021-08-29 19:05:06 +0200

    Update and rename sitioweb-python/index.py to app-web-python/index.py

--- app-web-python/index.py
+++ app-web-python/index.py
@@ -3,8 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route()
-def Inicio():
+def Inicio(/Inicio):
   return 'Hola Mundo'
+def Sobre Mi(/SobreMi):
+  return 'Sobre mí'
 
 if __name__ ==  '__main__':
   app.run()
